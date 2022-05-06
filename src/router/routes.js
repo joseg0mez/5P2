82660864906c09e76eddf9b6f19ce50d43457b2d
joseg0mez <josegomez@unomaha.edu>
2022-05-06 01:08:45 -0500

    update

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/PageHome.vue') }
+      { path: '', component: () => import('pages/PageHome.vue') },
+      { path: '/about', component: () => import('src/pages/PageAbout.vue') }
     ]
   },
 
